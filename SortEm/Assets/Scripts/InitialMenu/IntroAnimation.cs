785ac9f69d7d70d69fe658354c1b303423a8f1d0
Stephen Underwood <sunderwood@robotseamonstergames.com>
2017-08-08 18:10:03 -0700

    Folder restructure.  Brought in new art and text mesh pro plugin.

---
+++ SortEm/Assets/Scripts/InitialMenu/IntroAnimation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroAnimation : MonoBehaviour {
+
+	public IntroMoveOb squareOb;
+	public IntroMoveOb triangleOb;
+	public RectTransform squareSpawn;
+	public RectTransform triangleSpawn;
+	public RectTransform squareMove;
+	public RectTransform triangleMove;
+
+	void Awake(){
+		StartCoroutine ("SpawnSquare");
+		StartCoroutine ("SpawnTriangle");
+	}
+
+	IEnumerator SpawnSquare(){
+		yield return new WaitForSeconds (Random.Range (1, 3f));
+	}
+
+	IEnumerator SpawnTriangle(){
+		yield return new WaitForSeconds (Random.Range (1, 3f));
+	}
+
+}
